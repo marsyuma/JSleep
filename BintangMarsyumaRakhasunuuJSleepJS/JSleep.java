c47a164bbb22eaab65b274d0acff3a33acaea7dd
marsyuma <bintang.marsyuma@ui.ac.id>
2022-09-13 16:25:45 +0700

    second

---
+++ BintangMarsyumaRakhasunuuJSleepJS/JSleep.java
@@ -0,0 +1,35 @@
+package BintangMarsyumaRakhasunuuJSleepJS;
+
+
+/**
+ * Write a description of class JSleep here.
+ *
+ * @author Bintang Marsyuma Rakhasunu
+ * @version CS 1
+ */
+public class JSleep
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class JSleep
+     */
+    public JSleep()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
