bd7c53e247b2395c0c04b634e152e9379d8f5900
marsyuma <bintang.marsyuma@ui.ac.id>
2022-12-10 01:55:35 +0700

    Payment.json

--- src/main/java/com/BintangMarsyumaRakhasunuJSleepJS/Payment.java
+++ src/main/java/com/BintangMarsyumaRakhasunuJSleepJS/Payment.java
@@ -14,98 +14,77 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
-public class Payment extends Invoice {
-
-
-    private int roomId;
-    public Date from;
+public class Payment extends Invoice
+{
     public Date to;
+    public Date from;
+    private int roomId;
 
-
-    /**
-     * Constructs a `Payment` object with the given buyer ID, renter ID, room ID, start date, and end date.
-     *
-     * @param buyerId the ID of the buyer
-     * @param renterId the ID of the renter
-     * @param roomId the ID of the room being booked
-     * @param from the start date of the booking
-     * @param to the end date of the booking
-     */
-    public Payment(int buyerId, int renterId, int roomId, Date from, Date to) {
+    public Payment(int buyerId, int renterId,int roomId,Date from,Date to)
+    {
         super(buyerId, renterId);
-        this.roomId = roomId;
-        this.from = from;
         this.to = to;
+        this.from = from;
+        this.roomId = roomId;
     }
 
-    /**
-     * Constructs a `Payment` object with the given buyer account, renter, room ID, start date, and end date.
-     *
-     * @param buyer the buyer account
-     * @param renter the renter
-     * @param roomId the ID of the room being booked
-     * @param from the start date of the booking
-     * @param to the end date of the booking
-     */
-    public Payment(Account buyer, Renter renter, int roomId, Date from, Date to) {
-        super(buyer, renter);
-        this.roomId = roomId;
-        this.from = from;
+    public Payment(Account buyer,Renter renter,int roomId,Date from,Date to){
+        super(buyer,renter);
         this.to = to;
+        this.from = from;
+        this.roomId = roomId;
     }
 
-    /**
-     * Returns the ID of the room being booked.
-     *
-     * @return the room ID
-     */
-    public int getRoomId() {
+    public String print(){
+        return "Id :" + this.id + " BuyerId :" + buyerId + " RenterId : " + renterId +
+                " RoomId : " + roomId + " From : " + from + " To : " + to;
+    }
+
+    public int getRoomId(){
         return roomId;
     }
 
+
     public static boolean makeBooking(Date from,Date to,Room room){
         if(availability(from, to, room)){
-            Calendar start = Calendar.getInstance();
-            start.setTime(from);
-            Calendar end = Calendar.getInstance();
-            end.setTime(to);
-            for (Date date = start.getTime(); start.before(end); start.add(Calendar.DATE, 1), date = start.getTime()) {
-                room.booked.add(date);
+
+            while (from.before(to)){
+                room.booked.add(from); // Assign ke array
+                Calendar tempVar = Calendar.getInstance();
+                tempVar.setTime(from);
+                tempVar.add(Calendar.DATE, 1); //Increment 1
+                from = tempVar.getTime();
             }
             return true;
         }
-        return false;
-    }
-
-    public static boolean availability(Date from,Date to,Room room){
-        Calendar start = Calendar.getInstance();
-        start.setTime(from);
-        Calendar end = Calendar.getInstance();
-        end.setTime(to);
-        if(start.after(end) || start.equals(end)){
+        else{
             return false;
         }
-        for (Date date = start.getTime(); start.before(end); start.add(Calendar.DATE, 1), date = start.getTime()) {
-            if(room.booked.contains(date)){
+
+    }
+
+    //Todo : logic for checking availability and booking
+    public static boolean availability(Date from,Date to,Room room){
+
+
+        if(from.after(to) || from.equals(to)){
+            return false;
+        }
+
+        for (Date i : room.booked) {
+            if (from.equals(i)) {
                 return false;
+            } else if(from.before(i)){
+                if(from.before(i) && to.after(i)){
+                    return false;
+                }
             }
         }
         return true;
     }
 
-    /*public String getTime(){
-        SimpleDateFormat SDFormat = new SimpleDateFormat("'Formatted Date' = dd MMMM yyyy");
 
-    }*/
 
-    @Override
-    public String print(){
-        return "Payment{" +
-                "roomId=" + roomId +
-                ", from='" + from + '\'' +
-                ", to='" + to + '\'' +
-                '}';
-    }
 
 }
 
